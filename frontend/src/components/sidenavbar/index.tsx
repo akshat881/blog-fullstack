35509f31e74b60ae415066fbc48e0646fe41a617
akshat.ranjan <akshat.ranjan@thewitslab.com>
2023-04-05 21:04:48 +0530

    ui

--- frontend/src/components/sidenavbar/index.tsx
+++ frontend/src/components/sidenavbar/index.tsx
@@ -1,33 +1,126 @@
 import Bg from 'Assets/Logo/WITS@4x.svg'
-import { Container,Link,Ul,List,Hr} from 'styled/sidenavbar';
+import { Container,Link,Ul,List,Hr,Text,Head,Upernav,Navitem,Navtext,Sideitem,Input} from 'styled/sidenavbar';
+import { faNewspaper,faBars,faFilm,faMusic,faPlane,faUser, faCircleUser, faGear, faLifeRing, faArrowRightFromBracket, faComments, faBell, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 const Sidnav=()=>{
   
     return( 
       <>
       <Container>
-        <Ul>
-<List>
-<Link>
+<Ul>
+<List>     
+<Head>
 <img src={Bg} style={{maxWidth:"40%"}}/>
-</Link>
-</List>
-<Hr/>
-<List>
-<img src={Bg} style={{maxWidth:"50%"}}/>
-</List>
-<Hr/>
-<List>
+<FontAwesomeIcon icon={faBars} style={{color: "#a3a3a3",}} />
 
+</Head>
 </List>
 <Hr/>
+<List>
+<Head>
+<FontAwesomeIcon icon={faUser} size="2xl" style={{color: "#787878",}} />
+<Text>AKSHAT</Text>
+</Head>
+</List>
+<Hr left="7%" width="80%"/>
 <List>
   <Link>
-<img src={Bg} style={{maxWidth:"50%"}}/>
+  <FontAwesomeIcon icon={faNewspaper} style={{color: "#9c9c9c",}} />
+  <Text>News</Text>
+</Link>
+</List>
+<List>
+  <Link>
+  <FontAwesomeIcon icon={faFilm} style={{color: "#9c9c9c",}} />
+  <Text>Movie</Text>
+</Link>
+</List>
+<List>
+  <Link>
+  <FontAwesomeIcon icon={faMusic} style={{color: "#949494",}} />
+  <Text>Music</Text>
+</Link>
+</List>
+<List>
+  <Link>
+  <FontAwesomeIcon icon={faPlane} style={{color: "#545454",}} />
+  <Text>Travel</Text>
+</Link>
+</List>
+<Hr left="7%" width="80%"/>
+<List>
+  <Link>
+  <FontAwesomeIcon icon={faCircleUser} style={{color: "#787878",}} />
+  <Text>Account</Text>
+</Link>
+</List>
+<List>
+  <Link>
+  <FontAwesomeIcon icon={faGear} style={{color: "#8a8a8a",}} />
+  <Text>Setting</Text>
+</Link>
+</List>
+<List>
+  <Link>
+  <FontAwesomeIcon icon={faLifeRing} style={{color: "#999999",}} />
+  <Text>Help & Support</Text>
+</Link>
+</List>
+<List>
+  <Link>
+  <FontAwesomeIcon icon={faArrowRightFromBracket} style={{color: "#707070",}} />
+  <Text>Log Out</Text>
 </Link>
 </List>
         </Ul>
       </Container>
+      <Upernav>
+        <Navitem>
+        <Navtext>
+          <Link>
+          <Text>
+            Latest News
+          </Text>
+          </Link>
+         
+         
+          <Link>
+          <Text>
+          Political
+          </Text>
+          </Link>
+        
+        
+          <Link>
+          <Text>
+            Business
+          </Text>
+          </Link>
+        
+          <Link>
+          <Text>
+          Sports
+          </Text>
+          </Link>
+          <Link>
+          <Text>
+          Teach
+          </Text>
+          </Link>
+          </Navtext>
+          
+          <Sideitem>
+          <FontAwesomeIcon icon={faBell} style={{color: "#6e6e6e",borderRight:""}} />
+          <FontAwesomeIcon icon={faComments} style={{color: "#757575",}} />
+           <Input placeholder='Type to search'/>
+           <div style={{position:"relative"}} >
+           <FontAwesomeIcon icon={faMagnifyingGlass} style={{color: "#707070",position:"absolute",top:"-8px",right:"35px"}} />
+           </div>
+          </Sideitem>
+        </Navitem>
+      </Upernav>
         </>
     )
 }
--- frontend/src/styled/sidenavbar.tsx
+++ frontend/src/styled/sidenavbar.tsx
@@ -7,24 +7,79 @@ import styled from "styled-components";
  width:250px;
  height:100vh;
  border:1px solid #EEF1F2;
-
+overflow: hidden;
+ `
+ export const Head=styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+width: 90%;
  `
  export const Ul=styled.div`
-   margin-top:10%;
+position: absolute;
+left:0;
+ top:0;
+ width:100%;
+ height:100vh;
+
  `
  export const List=styled.div`
    margin-top:10%;
-   display:flex;
-   justify-content:center;
+ margin-left: 15%;
  `
- export const Hr=styled.div`
-   width:100%;
-   height:0;
-   border:1px solid #EEF1F2;
+ export const Hr=styled.div<{width?:string,left?:string}>`
 
+   width:${(props) => props.width};
+ margin-left: ${(props)=>props.left};
+   border:1px solid #EEF1F2;
+   margin-top: 10%;
  `
  export const Link=styled.a`
-   border:1px solid black;
-   padding:2rem 4rem;
+display: flex;
+align-items: center;
+   width: 100%;
+   cursor: pointer;
+ `
+ export const Text=styled.p`
+
+   padding:0.1rem 1rem;
    
  `
+ export const Upernav=styled.div`
+  position: absolute;
+  background: #fff;
+  height: 10.3vh;
+  top: 0;
+  right: 0;
+  width: calc(100% - 253px);
+  transition: all .5s ease-in-out;
+  z-index: 2;
+ display: flex;
+ align-items: center;
+ border-bottom:2px solid  #EEF1F2 ;
+   `
+ export const Navitem=styled.div`
+ display: flex;
+ align-items: center;
+ justify-content: space-between;
+ width: 100%;
+
+   `
+ export const Navtext=styled.div`
+ margin-left: 5%;
+ display: flex;
+width: 50%;
+   `
+ export const Sideitem=styled.div` 
+ gap:5%;
+ width: 30%;
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+   `
+ export const Input=styled.input`
+padding:0.8rem;
+width: 100%;
+border: 2px solid #EEF1F2;
+border-radius: 5px;
+   `
