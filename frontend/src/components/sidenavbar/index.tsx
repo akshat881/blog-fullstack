c8a32edbf8a4ca2e93289e9f95b1ff961cb7f6ad
akshat.ranjan <akshat.ranjan@thewitslab.com>
2023-04-11 19:22:36 +0530

    wits

--- frontend/src/components/sidenavbar/index.tsx
+++ frontend/src/components/sidenavbar/index.tsx
@@ -1,40 +1,39 @@
 import Bg from 'Assets/Logo/WITS@4x.svg'
 import {Form,Button,Cardcontent,Span,Tag,Ul2,Newslatest,Newcard,Newsupdate,News,Cardsinner,Container,Link,Ul,List,Hr,Text,Head,Upernav,Navitem,Navtext,Sideitem,Input,Mainsection,Cards, Cardimg} from 'styled/sidenavbar';
-import { faNewspaper,faBars,faFilm,faMusic,faPlane,faUser, faCircleUser, faGear, faLifeRing, faArrowRightFromBracket, faComments, faBell, faMagnifyingGlass, faArrowRight, faPlus, faHeart, faImage} from '@fortawesome/free-solid-svg-icons';
+import { faNewspaper,faBars,faFilm,faMusic,faPlane,faUser, faCircleUser, faGear, faLifeRing, faArrowRightFromBracket, faComments, faBell, faMagnifyingGlass, faArrowRight, faPlus, faHeart, faImage, faCircleXmark} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useGet,usePostdata} from 'hooks';
 import {useForm,SubmitHandler} from 'react-hook-form'
 import {useNavigate} from "react-router-dom";
 import axios from 'axios';
+import { useState } from 'react';
 
 const Sidnav=()=>{
+  
+  const [sowimg,setimg]=useState()
+  const [sow,set]=useState(false)
   const navigate = useNavigate();
-const myData=usePostdata();
-    // useGet("/logout")
- 
+const myData=usePostdata(); 
+const imgtext=(e:any)=>{
+   setimg(e.target.files[0].name)
+  console.log(e.target.files[0].name);
+  set(true)
+}
   const clearCookies =async()=>{
-
     await axios.get('http://localhost:4000/logout',{withCredentials:true}).then(()=>{
       window.location.href = "/";
     })
-     
   }
   const { register, handleSubmit} = useForm();
   const onSubmit=async(userpost:any)=>{ 
-    // console.log(userpost.imageData[0])
-    // const formData = {
-    //   title: userpost.name,
-    //   discription: userpost.discription,
-    //   imageData: userpost.imageData.file
-    // };
+
     const formData = new FormData();
     formData.append('title', userpost.title);
     formData.append('discription', userpost.discription);
     formData.append('imageData', userpost.imageData[0]);
     const response = await axios.post('http://localhost:4000/dash', formData);
     console.log(response);
-//     const data=await myData("/dash",formData)
-// console.log(formData)
+  
   }
     return( 
       <>
@@ -50,7 +49,7 @@ const myData=usePostdata();
 <Hr/>
 <List>
 <Head>
-{/* <FontAwesomeIcon icon={faUser} size="2xl" style={{color: "#787878",}} /> */}
+
 <img src="https://api.dicebear.com/6.x/bottts/png?seed=abhishake" width="50px" height="50px" alt="" />
 <Text>AKSHAT</Text>
 </Head>
@@ -162,16 +161,19 @@ const myData=usePostdata();
 <img src="https://api.dicebear.com/6.x/bottts/png?seed=abhishake" width="50px" height="50px" alt="" />
 <Input {...register("title")}style={{borderRadius:"50px",width:"90%",background:"#EEF1F2"}}/>
 </div>
-<textarea {...register("discription")}style={{borderRadius:"30px",marginTop:"2%",height:"20vw",border:"2px solid #EEF1F2"}}></textarea>
+<textarea {...register("discription")}style={{borderRadius:"30px",marginTop:"2%",height:"20vw",border:"2px solid #EEF1F2",padding:"2%"}}></textarea>
 <Hr style={{margin:"4%"}}/>
 
   <Tag style={{padding:"0",alignItems:"center",justifyContent:"space-around"}}>
+  <div style={{display:"flex",alignItems:"center",background:"#9a9a9a",color:"#fff",padding:"2%",borderRadius:"50px"}}>{sowimg}<FontAwesomeIcon icon={faCircleXmark} onClick={()=>set(false)}style={{color: "#ffffff",marginLeft:"5%",cursor:"pointer"}} /></div>
     <div style={{display:"flex",alignItems:"center",position:"relative"}}>
+
   <FontAwesomeIcon icon={faImage} style={{color: "#787878",zIndex:"1",cursor:"pointer"}} />
   <Text style={{cursor:"pointer"}}>Image</Text>
-  <input style={{opacity:"0",position:"absolute",cursor:"pointer"}} {...register("imageData")}type="file"/>
+  <input style={{opacity:"0",position:"absolute",cursor:"pointer",width:"100%"}} {...register("imageData")}type="file" onChange={imgtext}/>
  
   </div>
+
     <div style={{display:"flex",alignItems:"center"}}>
   <FontAwesomeIcon icon={faImage} style={{color: "#787878",}} />
   <Text style={{}}>Clip</Text>
--- frontend/src/helper/index.tsx
+++ frontend/src/helper/index.tsx
@@ -1,7 +1,7 @@
 import {useGet} from '../hooks'
 export const Check=()=>{
     const {data,isLoading}:any=useGet("/auth")
-console.log(isLoading)
+
 
     if(data?.data.message===true){
      return true
